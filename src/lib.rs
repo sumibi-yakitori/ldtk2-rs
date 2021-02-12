662c6313de1dcfd98ddefe602e5fab62b0be3a14
sumibi-yakitori <nekosoft360@gmail.com>
2021-02-12 15:45:38 +0900

    Update documents

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@
 //!
 //! # Usage
 //!
+//! ```sh
+//! cargo add ldtk2
+//! ```
+//!
 //! ```rust
 //! use std::error::Error;
 //!
@@ -20,7 +24,7 @@
 //!
 //! # Why did I create this nonsense?
 //!
-//! - [LDtk-rs](https://github.com/katharostech/LDtk-rs) uses code generation, it does not get autocomplete support from rust-analyzer. Also, there are special license restrictions on using that crate.
+//! - [LDtk-rs](https://github.com/katharostech/LDtk-rs) uses code generation, it does not get autocomplete support from rust-analyzer. Also, there are [special license](https://github.com/katharostech/katharos-license) restrictions on using that crate.
 //! - [ldtk_rust](https://github.com/estivate/ldtk_rust) uses `.except()` inside the crate, you can't handle the error.
 
 mod ldtk;
