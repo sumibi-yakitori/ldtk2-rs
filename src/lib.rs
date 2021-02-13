a8a617d023946503756447c54c93700190bc473f
sumibi-yakitori <nekosoft360@gmail.com>
2021-02-13 10:33:44 +0900

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 //! A thin crate for people who just want to use ldtk files freely.
 //!
 //!
+//! # Why did I create this nonsense?
+//!
+//! - [LDtk-rs](https://github.com/katharostech/LDtk-rs) uses code generation, it does not get autocomplete support from rust-analyzer. Also, there are [special license](https://github.com/katharostech/katharos-license) restrictions on using that crate.
+//! - [ldtk_rust](https://github.com/estivate/ldtk_rust) uses `.except()` inside the crate, you can't handle errors.
+//!
+//!
+//! # Supported LDtk file versions
+//!
+//! `>=0.7.2`
+//!
+//!
 //! # Usage
 //!
 //! ```sh
@@ -26,12 +37,6 @@
 //!   Ok(())
 //! }
 //! ```
-//!
-//!
-//! # Why did I create this nonsense?
-//!
-//! - [LDtk-rs](https://github.com/katharostech/LDtk-rs) uses code generation, it does not get autocomplete support from rust-analyzer. Also, there are [special license](https://github.com/katharostech/katharos-license) restrictions on using that crate.
-//! - [ldtk_rust](https://github.com/estivate/ldtk_rust) uses `.except()` inside the crate, you can't handle errors.
 
 mod ldtk;
 pub use ldtk::*;
